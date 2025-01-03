e2a3cb9a5487d78f139a0478fc3f50481bf19917
LachlanBligh <lachlanbligh03@gmail.com>
2025-01-04 01:59:00 +1000

    ss

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio-project",
+  base: "/", // For GitHub Pages: set to "/repo-name/"
 });
